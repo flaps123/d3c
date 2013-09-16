62a5b1b756dc647987ff26c750c3307034d7e3b3
Eric Shull <shull@atomicobject.com>
2013-09-16 06:16:50 -0400

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject d3c "0.1.0-SNAPSHOT"
+  :description "D3 library for ClojureScript"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [net.drib/strokes "0.5.0"]])
