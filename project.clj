2cd7f95a0c60f2bbf3fe556d052a257629e87e9d
Eric Shull <shull@atomicobject.com>
2014-02-10 07:21:44 -0500

    Change project URL to GitHub from Clojars.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject d3c "0.2.0"
   :description "D3 library for ClojureScript"
-  :url "http://clojars.org/d3c"
+  :url "https://github.com/exupero/d3c"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
