eccd1c713212a4eaa647e2fc5d81cf288ce6e01b
Eric Shull <shull@atomicobject.com>
2014-07-21 20:55:22 -0400

    Bump version and remove duplicate license info.

--- project.clj
+++ project.clj
@@ -1,11 +1,9 @@
-(defproject d3c "0.2.3-SNAPSHOT"
+(defproject d3c "0.3.0-SNAPSHOT"
   :description "D3 library for ClojureScript"
   :url "https://github.com/exupero/d3c"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [net.drib/strokes "0.5.1"]]
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
         :url "https://github.com/exupero/d3c"})
